20af3eef61c3aa6811f0cc44ca0322f86ede152c
kitokengo <ki1980to@gmail.com>
2023-11-28 15:04:25 +0900

    適切なコミットコメント

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,7 +9,7 @@ class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return view('posts/index')->with(['posts' => $post->getPaginateByLimit()]);
+        return view('posts/index')->with(['posts' => $post->getPaginateByLimit(1)]);
     }
     public function show(Post $post)
     {
@@ -19,6 +19,13 @@ class PostController extends Controller
     public function create()
     {
         return view('posts/create');
+    }
+    
+    public function store(Request $request, Post $post)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' .$post->id);
     }
 }
 ?>
