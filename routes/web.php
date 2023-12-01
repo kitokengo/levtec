780488705635cfa43fe9a057af83a2168e47ec14
kitokengo <ki1980to@gmail.com>
2023-12-01 15:14:15 +0900

    適切なコミットコメント

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,12 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-<<<<<<< Updated upstream
-=======
 Route::get('/', [PostController::class, 'index']);
 Route::get('/posts/create',[PostController::class , 'create']);
 Route::get('/posts/{post}', [PostController::class ,'show']);
 Route::post('/posts' , [PostController::class , 'store']);
 Route::get('/posts/{post}/edit' , [PostController::class , 'edit']);
 Route::put('/posts/{post}' , [PostController::class , 'update']);
->>>>>>> Stashed changes
